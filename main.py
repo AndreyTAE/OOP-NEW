00a26eee4951f0f39ec6783e00ef0220458ce0fd
Andrey <troeglazoofcar@gmail.com>
2025-02-21 09:06:40 +0300

    Добавлены магические методы и возможность сравнения

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
-            if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
-            else:
-                lecturer.grades[course] = [grade]
-        else:
-            return "Ошибка"
+    def __str__(self):
+        avg_grade = self.get_avg_grade()
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания: {avg_grade}\n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {', '.join(self.finished_courses)}")
+
+    def get_avg_grade(self):
+        if not self.grades:
+            return 0
+        return sum(sum(grades) for grades in self.grades.values()) / sum(len(grades) for grades in self.grades.values())
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.get_avg_grade() < other.get_avg_grade()
+        return NotImplemented
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -22,20 +32,36 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
-        else:
-            return "Ошибка"
 
-class Reviewer(Mentor):
-    def rate_hw(self, student, course, grade):
-        super().rate_hw(student, course, grade)
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def __str__(self):
+        avg_grade = self.get_avg_grade()
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {avg_grade}")
+
+    def get_avg_grade(self):
+        if not self.grades:
+            return 0
+        return sum(sum(grades) for grades in self.grades.values()) / sum(len(grades) for grades in self.grades.values())
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.get_avg_grade() < other.get_avg_grade()
+        return NotImplemented
