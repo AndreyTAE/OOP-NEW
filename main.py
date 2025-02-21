21f13bcd3794d922322f7017b069dca613b2471d
Andrey <troeglazoofcar@gmail.com>
2025-02-21 08:44:14 +0300

    Добавлены классы Lecturer и Reviewer с методами для выставления оценок

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка"
 
 class Mentor:
     def __init__(self, name, surname):
@@ -17,37 +25,17 @@ class Mentor:
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
-                student.grades[course].append(grade)
+                student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
             return "Ошибка"
 
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        super().rate_hw(student, course, grade)
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        # Дополнительные атрибуты для лекторов могут быть добавлены здесь
-
-
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        # Дополнительные атрибуты для экспертов могут быть добавлены здесь
-
-
-# Пример использования
-best_student = Student("Ruoy", "Eman", "your_gender")
-best_student.courses_in_progress += ["Python"]
-
-cool_lecturer = Lecturer("Some", "Buddy")
-cool_lecturer.courses_attached += ["Python"]
-
-cool_reviewer = Reviewer("John", "Doe")
-cool_reviewer.courses_attached += ["Python"]
-
-cool_lecturer.rate_hw(best_student, "Python", 10)
-cool_lecturer.rate_hw(best_student, "Python", 10)
-cool_lecturer.rate_hw(best_student, "Python", 10)
-
-print(best_student.grades)
+        self.grades = {}
